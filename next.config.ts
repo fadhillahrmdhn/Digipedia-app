be34a2ccf20215deeb5ee519a486e699665e2279
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2025-09-08 18:58:48 +0700

    feat: update dependencies and add axios configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: path.join(__dirname, ".."),
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "digi-api.com",
+        port: "",
+        pathname: "/images/digimon/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
